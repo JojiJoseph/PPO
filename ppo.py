48bdb57bd848b7e8ca25b1ab08ab75e4497afe0f
joji <jojijoseph@iisc.ac.in>
2021-05-21 15:12:43 +0530

    Sped up ppo

--- ppo.py
+++ ppo.py
@@ -17,7 +17,7 @@ from rollout_buffer import RolloutBuffer
 # torch.set_default_tensor_type('torch#.cuda.FloatTensor')
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# device = "cpu"
+device = "cpu"
 print("Using device:", device)
 
 class Actor(nn.Module):
@@ -54,7 +54,8 @@ class ActorCritic(nn.Module):
 
 class PPO():
     def __init__(self, actor=None, critic=None, learning_rate=1e-3, env_name="CartPole-v1",
-        n_timesteps=int(1e6), batch_size=64, n_epochs=10, n_rollout_timesteps=1024, coeff_v=0.5):
+        n_timesteps=int(1e6), batch_size=64, n_epochs=10, n_rollout_timesteps=1024, coeff_v=0.5,
+        clip_range=0.2,n_eval_episodes=5):
 
         self.LEARNING_RATE = 1e-3
         self.ENV_NAME = env_name
@@ -63,9 +64,13 @@ class PPO():
         self.N_EPOCHS = n_epochs
         self.N_ROLLOUT_TIMESTEPS = n_rollout_timesteps
         self.COEFF_V = coeff_v
+        self.CLIP_RANGE = clip_range
+        self.N_EVAL_EPISODES = n_eval_episodes
 
     def learn(self):
         env = gym.make(self.ENV_NAME)
+
+        self.env = env
 
         print(env.action_space)
         print(env.observation_space)
@@ -103,7 +108,8 @@ class PPO():
             self.buffer.clear()
             
             _state = env.reset() # Unconverted state
-            
+
+            t_train_start = time.time()
             while rollout_timesteps < self.N_ROLLOUT_TIMESTEPS:
                 with torch.no_grad():
                     state = _state[None,:]
@@ -132,7 +138,7 @@ class PPO():
                 last_value = last_value[0].cpu().numpy().item()
 
             self.buffer.compute_values(last_value)
-            print("Collection time", time.time()-rollout_start_time)
+            # print("Collection time", time.time()-rollout_start_time)
             for epoch in range(self.N_EPOCHS):
                 for states, actions, values, old_log_prob in self.buffer:
 
@@ -141,74 +147,77 @@ class PPO():
                     states = torch.as_tensor(states).to(device)
                     values = torch.as_tensor(values).flatten().to(device)
                     old_log_prob = torch.as_tensor(old_log_prob).to(device)
-                    # opt_critic.zero_grad()
-                    # V = critic(states).flatten()
-                    # loss = self.COEFF_V * F.mse_loss(V,values)
-                    # loss.backward()
-                    # opt_critic.step()
-
-                    # V = V.detach()
-
-                    # advantages = values - V
-                    # advantages = (advantages - advantages.mean())/(advantages.std() + 1e-8)
-                    # advantages = advantages.squeeze()
 
                     opt.zero_grad()
                     action_params, V = actor_critic(states)
                     V = V.flatten()
-                    # print(V.shape, values.shape)
-                    value_loss = self.COEFF_V * F.mse_loss(V,values)
+
+                    loss_critic = self.COEFF_V * F.mse_loss(V,values)
+                    
                     advantages = values - V.detach()
                     advantages = (advantages - advantages.mean())/(advantages.std() + 1e-8)
-                    advantages = advantages.squeeze()
-                    # print(advantages.shape)
+                    advantages = advantages.flatten()
+
                     log_prob = torch.distributions.Categorical(logits=action_params).log_prob(actions)
-                    # print(log_prob.shape)
+
                     ratio = torch.exp(log_prob - old_log_prob).squeeze()
                     l1 = ratio*advantages
-                    l2 = torch.clip(ratio, 0.8,1.2)*advantages
-                    # print(l1.shape, l2.shape, value_loss.shape)
-                    loss = -torch.min(l1,l2)
-                    loss = loss.sum() + value_loss
-                    # print(loss)
+                    l2 = torch.clip(ratio, 1 - self.CLIP_RANGE, 1 + self.CLIP_RANGE)*advantages
+                    loss_actor = -torch.min(l1,l2)
+                    loss = loss_actor.mean() + loss_critic
                     loss.backward()
                     opt.step()
             self.buffer.clear()
             # Evaluation step
             iteration += 1
             total_reward = 0
-            for episode in range(10):
-                _state = env.reset()
-                done = False
-                while not done:
-                    state = _state[None,:]
-                    with torch.no_grad():
-                        state = torch.as_tensor(state).float().to(device)
+            t_train_end = time.time()
+            self.actor_critc = actor_critic
+            print("\nIteration = ", iteration)
+            if iteration % 10 == 1:
+                t_evaluation_start = time.time()
+                evaluation_score = self.evaluate()
+                t_evaluation_end = time.time()
+                print("evaluation_time = ", t_evaluation_end - t_evaluation_start)
+                print("Avg. Return - evaluation = ", evaluation_score)
+            print("Training time = ", t_train_end - t_train_start)
+    def evaluate(self):
+        total_reward = 0
+        env = self.env
+        actor_critic = self.actor_critc
+        for episode in range(self.N_EVAL_EPISODES):
+            _state = env.reset()
+            done = False
+            while not done:
+                state = _state[None,:]
+                with torch.no_grad():
+                    state = torch.as_tensor(state).float().to(device)
 
-                        action_params, _ = actor_critic(state)
-                        action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))[0]
-                        # action = torch.argmax(torch.softmax(action_params[0],-1))
-                        action = action.cpu().numpy()
-                    next_state, reward, done, info = env.step(action)
-                    _state = next_state
-                    total_reward += reward
-            print(iteration,episodes_passed, total_timesteps, "avg reward", total_reward/10)
-            # torch.save(actor.state_dict(), "./actor_cartpole.pt")
-            # torch.save(critic.state_dict(), "./critic_cartpole.pt")
-            print(time.time()-rollout_start_time)
-            # state = env.reset()
-            # done = False
-            # while not done:
-            #     state = state[None,:]
-            #     state = torch.tensor(state).float()#.cuda()
+                    action_params, _ = actor_critic(state)
+                    action = torch.argmax(torch.softmax(action_params[0],-1))
+                    # action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))[0]
+                    action = action.cpu().numpy()
+                next_state, reward, done, info = env.step(action)
+                _state = next_state
+                total_reward += reward
+        return total_reward / self.N_EVAL_EPISODES
+        print(iteration,episodes_passed, total_timesteps, "avg reward", total_reward/self.N_EVAL_EPISODES)
+        # torch.save(actor.state_dict(), "./actor_cartpole.pt")
+        # torch.save(critic.state_dict(), "./critic_cartpole.pt")
+        print(time.time()-rollout_start_time)
+        # state = env.reset()
+        # done = False
+        # while not done:
+        #     state = state[None,:]
+        #     state = torch.tensor(state).float()#.cuda()
 
-            #     action_params = actor(state)
-            #     action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))
-            #     action = action[0].detach().cpu().numpy()
-            #     env.render()
-            #     time.sleep(1/30.)
-            #     next_state, reward, done, info = env.step(action)
-            #     state = next_state
-            # env.close()
+        #     action_params = actor(state)
+        #     action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))
+        #     action = action[0].detach().cpu().numpy()
+        #     env.render()
+        #     time.sleep(1/30.)
+        #     next_state, reward, done, info = env.step(action)
+        #     state = next_state
+        # env.close()
 
-            # total_timesteps += 1
+        # total_timesteps += 1
--- train.py
+++ train.py
@@ -5,9 +5,9 @@ Utility script to train diff gym compatible libraries with ppo
 """
 
 from ppo import PPO
-from ppo_old import PPO
+# from ppo_old import PPO
 
-algo = PPO(learning_rate=1e-3, n_timesteps= 1e6, n_epochs=10, batch_size=128, n_rollout_timesteps=256)
+algo = PPO(learning_rate=1e-3, n_timesteps= 1e6, n_epochs=10, batch_size=256, n_rollout_timesteps=512)
 
 algo.learn()
 
