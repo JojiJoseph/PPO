6968aecbb8028d00d0c0a57821336c4cc7a70413
joji <jojijoseph@iisc.ac.in>
2021-05-25 01:29:53 +0530

    Implemented loading hyperparameters from a yaml file

--- train.py
+++ train.py
@@ -3,10 +3,24 @@
 Utility script to train diff gym compatible libraries with ppo
 
 """
-
+import argparse
+import yaml
 from ppo import PPO
 
-algo = PPO(learning_rate=1e-3, n_timesteps= 1e6, n_epochs=10, batch_size=256, n_rollout_timesteps=512, device="cpu", coeff_entropy=0.01)
+parser = argparse.ArgumentParser()
+parser.add_argument("-e","--exp",type=str, required=True,help="The experiment name as defined in the yaml file")
+
+with open("./experiments.yaml") as f:
+    experiments = yaml.safe_load(f)
+
+args = parser.parse_args()
+
+print(args)
+
+experiment = args.exp
+hyperparams = experiments[experiment]
+
+algo = PPO(device="cpu",**hyperparams)
 
 algo.learn()
 
