ebc5f84dc2c36337944d45bc7a091c540d47e10a
joji <jojijoseph@iisc.ac.in>
2021-05-28 01:01:05 +0530

    Created a play.py file for all experiments

--- play.py
+++ play.py
@@ -1,28 +1,83 @@
 import gym
 import torch
 import time
+import argparse
+from gym.wrappers import Monitor
+import yaml
+import pybullet_envs
+n_episodes = 1
 
-n_episodes = 2
+parser = argparse.ArgumentParser()
+parser.add_argument("-e","--exp",type=str, required=True,help="The experiment name as defined in the yaml file")
 
 fps = 30
-from net import ActorCritic
+from net import ActorCritic, ActorCriticContinuous
+with open("./experiments.yaml") as f:
+    experiments = yaml.safe_load(f)
 
-env = gym.make("CartPole-v1")
+args = parser.parse_args()
 
-actor_critic = ActorCritic(4, 2)
-actor_critic.load_state_dict(torch.load("./CartPole-v1.pt"))
+print(args)
 
+experiment = args.exp
+print(experiment)
+hyperparams = experiments[experiment]
+print(hyperparams)
+env = Monitor(gym.make(hyperparams['env_name']), './video', force=True)
+
+state_dim = env.observation_space.shape[0]
+if type(env.action_space) == gym.spaces.Discrete:
+    n_actions = env.action_space.n
+    actor_critic = ActorCritic(state_dim, n_actions)
+elif type(env.action_space) == gym.spaces.Box:
+    action_dim = env.action_space.shape[0]
+    actor_critic = ActorCriticContinuous(state_dim, action_dim)
+
+# actor_critic = ActorCritic(4, 2)
+actor_critic.load_state_dict(torch.load("./results/" + experiment + "/model.pt"))
+
+obs_normalization = None
+obs_shift = 0
+obs_scale = 0
+if "obs_normalization" in hyperparams:
+    obs_normalization = hyperparams["obs_normalization"]
+if "obs_shift" in hyperparams:
+    obs_shift = hyperparams["obs_shift"]
+if "obs_scale" in hyperparams:
+    obs_scale = hyperparams["obs_scale"]
+
+def normalize_obs(observation):
+    if obs_normalization == "simple":
+        if obs_shift is not None:
+            observation += obs_shift
+        if obs_scale is not None:
+            observation /= obs_scale
+    return observation
+try:
+    env.render()
+except:
+    pass
 state = env.reset()
 done = False
+total_reward = 0
 while not done:
+    state = normalize_obs(state)
     state = state[None,:]
     state = torch.tensor(state).float()#.cuda()
 
     action_params, _ = actor_critic(state)
-    action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))
+    if type(env.action_space) == gym.spaces.Discrete:
+        action = torch.distributions.Categorical(logits=action_params[0]).sample((1,))
+    else:
+        mu, log_sigma = action_params
+        distrib = torch.distributions.Normal(mu[0], log_sigma.exp())
+        action = distrib.sample((1,))
     action = action[0].detach().cpu().numpy()
     env.render()
     time.sleep(1/fps)
     next_state, reward, done, info = env.step(action)
     state = next_state
+    total_reward += reward
 env.close()
+
+print("Total Reward:", total_reward)
